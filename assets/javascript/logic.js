0547c1f18922578ae3d3c7b6aaa8f6fa0ad111af
Victor Ha <34376698+havictor@users.noreply.github.com>
2018-05-20 16:19:59 -0400

    Update logic.js
    Added the ability to grief

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,5 +12,19 @@ let database=firebase.database();
 
 let username=prompt("What is your name?").trim();
 
+database.ref().on("child_added", function(childsnapshot) {
+  var previousText = griefing.text();
+  griefing.text(`${previousText} /n ${childsnapshot.val().username}: ${childsnapshot.val().message}`);
+});
+
+$("#grief").on("click", function(event) {
+  event.preventDefault();
+  var message = $("#BM").val().trim();
+  database.ref().push({
+    username: username,
+    message: message
+  })
+});
+
 var userSelection
 var enemySelection
