7cd4c9c2afcd2df37628918e677f9160e9e636ed
Ha <victorha@gmail.com>
2018-05-29 00:22:07 -0400

    added game logic, children still disobedient

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,149 +1,209 @@
-  var config = {
-    apiKey: "AIzaSyBUb0nTz38498BfIgXvzS_cikLWfqEQoAk",
-    authDomain: "rps-multiplayer-9dac6.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-9dac6.firebaseio.com",
-    projectId: "rps-multiplayer-9dac6",
-    storageBucket: "",
-    messagingSenderId: "435987726360"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyBUb0nTz38498BfIgXvzS_cikLWfqEQoAk",
+  authDomain: "rps-multiplayer-9dac6.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-9dac6.firebaseio.com",
+  projectId: "rps-multiplayer-9dac6",
+  storageBucket: "",
+  messagingSenderId: "435987726360"
+};
+firebase.initializeApp(config);
 
 let database=firebase.database();
-//let database1=firebase.database().ref("player1");//
-//let database2=firebase.database().ref("player2");//
 
 let username=$.trim(prompt("What is your name?"));
 
 if (username === "") {
-  $("#greeting").text("Welcome, agent of the House of Black and White");
+$("#greeting").text("Welcome, agent of the House of Black and White");
 }
 else $("#greeting").text("Welcome, "+username+"!");
 
 var player
-var playerCount
+var player1seat
+var player2seat
+var userSelection
+var enemySelection
 
 $("#seat").on("click", function() {
+  event.preventDefault();
   checkOpen();
 });
 
-// var connect = database.ref("user");
-//   if (player = "Player 1") {
-//     connect.update({
-//       player1name: "",
-
-//     })
-//   }
-//   connect.update({
-//     playerCount
-//   })
-//   playerCount: playerCount--
-  
-// })
-// //
+var gameSelection = database.ref("/game");
+gameSelection.on("child_changed", function(childsnapshot) {
+  console.log("catch");
+  if (player == "Player 1") {
+    console.log("player2")
+    //console.log(children.child("player2").val().player2choice)
+    console.log(database.ref("\/game\/player2").val().player2choice)
+    //enemySelection = children.child("player2").val().player2choice
+  }
+  else if (player == "Player 2") {
+    console.log("player1")
+    //console.log(children.child("player1").val().player1choice)
+    console.log(database.ref("\/game\/player1").val().player1choice)
+    //enemySelection = children.child("player1").val().player1choice
+  }
+  if (playerSelection == "scissors") {
+    if (enemySelection == "rock") {
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: `has lost using ${playerSelection}`
+      })
+    }
+    else if (enemySelection == "paper") {
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: `has won using ${playerSelection}`
+      })
+    }
+    else if (enemySelection == "scissors") {
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: `has drew using ${playerSelection}`
+      })
+    }
+  }
+  else if (playerSelection == "rock") {
+    if (enemySelection == "rock") {
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: `has drew using ${playerSelection}`
+      })
+    }
+    else if (enemySelection == "paper") {
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: `has lost using ${playerSelection}`
+      })
+    }
+    else if (enemySelection == "scissors") {
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: `has won using ${playerSelection}`
+      })
+    }
+  }
+  else if (playerSection == "paper") {
+    if (enemySelection == "rock") {
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: `has won using ${playerSelection}`
+      })
+    }
+    else if (enemySelection == "paper") {
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: `has drew using ${playerSelection}`
+      })
+    }
+    else if (enemySelection == "scissors") {
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: `has lost using ${playerSelection}`
+      })
+    }
+  }
+})
 
 function checkOpen() {
-  var connect = database.ref("user");
-  //var chatConnect = database.ref("chat").push();
-    connect.once("value").then(function(snapshot) {
-      playerCount = snapshot.val().playerCount;
-      if ((snapshot.val().playerCount == 0) && (player !== "Player 1") && (player !== "Player 2")) {
-        player = "Player 1"
-        playerCount++
-        database.ref("user").update({
-          playerCount: playerCount,
-          player1name: username
-        });
-        database.ref("chat").push({
-          username: player+" "+username,
-          message: " has entered the game"
-        })
-    //should reset ondisconnect
-    connect.onDisconnect().update({
-      playerCount: (playerCount-1),
+var connect1 = database.ref("user");
+var connect2 = database.ref("user");
+  connect1.once("value").then(function(snapshot) {
+    player1seat = snapshot.val().player1seat;
+    player2seat = snapshot.val().player2seat;
+    if ((player1seat != false) && (player !== "Player 1") && (player !== "Player 2")) {
+      player = "Player 1"
+      player1seat = false;
+      database.ref("user").update({
+        player1seat: player1seat,
+        player1name: username
+      });
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: " has entered the game"
+      })
+    connect1.onDisconnect().update({
+      player1seat: true,
       player1name: ""
     })
-    var chatConnect = database.ref("chat").push();
-    chatConnect.onDisconnect().set({
+    var chatConnect1 = database.ref("chat").push();
+    chatConnect1.onDisconnect().set({
       username: player+" "+username,
       message: " has left the game"
     })
-      }
-      else if ((snapshot.val().playerCount == 1) && (player !== "Player 1") && (player !== "Player 2")) {
-        player ="Player 2"
-        playerCount++
-        database.ref("user").update({
-          playerCount: playerCount,
-          player2name: username
-        });
-        database.ref("chat").push({
-          username: player+" "+username,
-          message: " has entered the game"
-        })
-        connect.onDisconnect().update({
-          playerCount: (playerCount-1),
-          player2name: ""
-        })
-      }
-      else if ((player == "Player 1") || (player == "Player 2")) {
-        alert("You already have a seat, stop trying to hog all the chairs!");
-      }
-      else alert("There are no free spots at the table");
-    });
+    }
+    else if ((player2seat != false) && (player !== "Player 1") && (player !== "Player 2")) {
+      player ="Player 2"
+      player2seat = false;
+      database.ref("user").update({
+        player2seat: player2seat,
+        player2name: username
+      });
+      database.ref("chat").push({
+        username: player+" "+username,
+        message: " has entered the game"
+      })
+      connect2.onDisconnect().update({
+        player2seat: true,
+        player2name: ""
+      })
+      var chatConnect2 = database.ref("chat").push();
+      chatConnect2.onDisconnect().set({
+        username: player+" "+username,
+        message: " has left the game"
+      })
+    }
+    else if ((player == "Player 1") || (player == "Player 2")) {
+      alert("You already have a seat, stop trying to hog all the chairs!");
+    }
+    else alert("There are no free spots at the table");
+  });
 }
 
 //make the colon a variable to prevent it from showing upon connect/disconnect
 database.ref("chat").on("child_added", function(childsnapshot) {
-  var previousText = $("#griefing").text();
-  $("#griefing").text(`${previousText} \n ${childsnapshot.val().username}: ${childsnapshot.val().message}`);
+var previousText = $("#griefing").text();
+$("#griefing").text(`${previousText} \n ${childsnapshot.val().username}: ${childsnapshot.val().message}`);
 });
 
 $("#grief").on("click", function(event) {
-  event.preventDefault();
-  var message = $("#BM").val().trim();
-  if (message != "") {
-    database.ref("chat").push({
-      username: username,
-      message: message
-    })
-    $("#BM").val("");
-  }
+event.preventDefault();
+var message = $("#BM").val().trim();
+if (message != "") {
+  database.ref("chat").push({
+    username: username,
+    message: message
+  })
+  $("#BM").val("");
+}
 });
 
-
-var userSelection
-var enemySelection
-
-database.ref("game").on("value", function(snap) {
-  //change enemySelection
-})
-
 $(".choice").on("click", function() {
-  if (userSelection === "") {
+  if ((userSelection == "") || (userSelection == undefined)) {
     userSelection=$(this).attr("id");
     if (player == "Player 1") {
-      database.ref("game").update({
+      database.ref("game\/player1").set({
         player1choice: userSelection
       })
-      
     }
     else if (player == "Player 2") {
-      database.ref("game").update({
+      database.ref("game\/player2").set({
         player2choice: userSelection
       })
-
     }
-  //push choice to server
-  //compare yours & enemy's choice via if statements (player 1 vs player 2?)
-  //declare winner in chat
-  
-  
-  //need a promise where wait for enemySelecction, andthen reset
-  userSelection="";
+//push choice to server
+//compare yours & enemy's choice via if statements (player 1 vs player 2?)
+//declare winner in chat
+
+
+//need a promise where wait for enemySelecction, andthen reset
+//userSelection="";
   }
 });
 
 database.ref("user").on("value", function(snaps) {
-  playerCount=snaps.val().playerCount;
+  player1seat=snaps.val().player1seat;
+  player2seat=snaps.val().player2seat;
 })
 
 //need to fix autoscroll.
